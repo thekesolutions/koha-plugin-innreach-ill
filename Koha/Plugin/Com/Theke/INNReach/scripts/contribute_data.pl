96fee798edb63be5a8ec0cd21cdc0a9b95bfab1a
Tomas Cohen Arazi <tomascohen@theke.io>
2019-06-03 12:27:59 -0300

    Issue #42: Add --locations to contribute_data.pl
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- Koha/Plugin/Com/Theke/INNReach/scripts/contribute_data.pl
+++ Koha/Plugin/Com/Theke/INNReach/scripts/contribute_data.pl
@@ -15,11 +15,13 @@ my $biblio_id;
 my $all           = 0;
 my $noout         = 0;
 my $exclude_items = 0;
+my $locations     = 0;
 
 my $result = GetOptions(
     'biblio_id=s'   => \$biblio_id,
     'all'           => \$all,
     'exclude_items' => \$exclude_items,
+    'locations'     => \$locations,
     'noout'         => \$noout,
 );
 
@@ -41,7 +43,8 @@ sub print_usage {
     --biblio_id     Only contribute the specified biblio_id
     --all           Contribute all records
     --exclude_items Exclude items from this batch update
-    --noout         No generar output de registros
+    --locations     Update Central server's locations
+    --noout         No output
 
 Note: --biblio_id and --all are mutually exclussive
 
@@ -64,6 +67,10 @@ elsif ($all) {
         $contribution->contribute_batch_items( { bibId => $biblio->biblionumber } )
             unless $exclude_items;
     }
+}
+
+if ( $locations ) {
+    $contribution->upload_locations_list();
 }
 
 1;
