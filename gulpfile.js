cb446a1cd7049944e235369a9f6d014515e4dca3
Tomas Cohen Arazi <tomascohen@theke.io>
2024-12-16 09:01:38 -0300

    Issue #291: Streamline `npm version` workflow setting the version
    This patch makes the `npm version` release workflow hardcode the version
    in the code. This helps testing.
    
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- gulpfile.js
+++ gulpfile.js
@@ -131,7 +131,6 @@ function build() {
     return run(`
         mkdir dist ;
         cp -r Koha dist/. ;
-        sed -i -e "s/{VERSION}/${package_json.version}/g" ${pm_file_path_full_dist} ;
         sed -i -e "s/1980-06-18/${today}/g" ${pm_file_path_full_dist} ;
         cd dist ;
         zip -r ../${release_filename} ./Koha ;
